1d970b6c43b8acd4a52c12b8b4df0cd93225f56f
Gaurav <chandanigaurav.rkc@gmail.com>
2017-04-11 19:21:07 +1200

    changes added

--- web.js
+++ web.js
@@ -1,7 +1,13 @@
 var gzippo = require('gzippo');
   var express = require('express');
+  var bodyParser = require('body-parser');
+  var logger = require('morgan');
+
+
   var app = express();
 
-  app.use(express.logger('dev'));
+  app.use(logger('dev'));
   app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+  // parse application/json
+    app.use(bodyParser.json());
   app.listen(process.env.PORT || 5000);
